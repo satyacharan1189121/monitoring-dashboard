f40f98b189be44dcfffbb9d2e1d3bf02adffd8a4
Satya <mandasatyacharanreddy@gmail.com>
2026-04-27 00:14:53 +0530

    Update MonitoringDashboardApplication.java

--- src/main/java/com/monitoring/dashboard/MonitoringDashboardApplication.java
+++ src/main/java/com/monitoring/dashboard/MonitoringDashboardApplication.java
@@ -2,12 +2,18 @@ package com.monitoring.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+    }
+)
 public class MonitoringDashboardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MonitoringDashboardApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringDashboardApplication.class, args);
+    }
 }
